36b0c344702a1c7666978ba730255171b9ebbfc0
Tyler <robinsontj15@gmail.com>
2022-10-07 12:02:15 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,49 @@
 
+let questions = [
+    {
+    numb: 1,
+    question: "do you like dogs?",
+    answer: "yes",
+    options: [
+      "yes ",
+      "no",
+    ]
+  },
+    {
+    numb: 2,
+    question: "do you like cats",
+    answer: "no",
+    options: [
+      "yes",
+      "no",
+    ]
+  },
+    {
+    numb: 3,
+    question: "does tom like turtles?",
+    answer: "yes",
+    options: [
+      "yes",
+      "no",
+
+    ]
+  },
+    {
+    numb: 4,
+    question: "is fire hot?",
+    answer: "yes",
+    options: [
+      "yes",
+      "no",
+    ]
+  },
+    {
+    numb: 5,
+    question: "are cats evil?",
+    answer: "yes",
+    options: [
+      "yes",
+      "no",
+    ]
+  },
+];
